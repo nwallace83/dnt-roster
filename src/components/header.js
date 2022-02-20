2b5676756866ba8f1de7b3709db3f6698e38b111
Nathan Wallace <nwallace6988@gmail.com>
2022-02-19 19:27:59 -0700

    start building discord integration

--- src/components/header.js
+++ src/components/header.js
@@ -1,21 +1,36 @@
 import React from 'react';
 import logo from '../images/logo.jpg'
+import discordLogo from '../images/discordLogo.jpg'
 
 class Header extends React.Component {
     render() {
         return (
-            <div className="row">
-                <div className="col-md-1">
-                 <img src={logo} height='40px' />
-                </div>
-                <div className="col-md-8">
-                    <ul className="nav nav-tabs">
-                        <li className="nav-item">
-                            <a className="nav-link active" aria-current="page" href="#">Roster</a>
-                        </li>
-                    </ul>
+            <div className="container">
+                <div className="row">
+                    <div className="col-md-8" id="nav-menu">
+                        <ul className="nav nav-tabs">
+                            <li className="nav-item">
+                                <a className="nav-link active" aria-current="page" href="#" id="company-logo" style={{ backgroundImage: `url(${logo})`, backgroundSize: 'contain' }}>
+                                    {/* <img src={logo} height="20px" id="company-logo" /> */}
+                                </a>
+                            </li>
+                            <li className="nav-item">
+                                <a className="nav-link active" aria-current="page" href="#">Roster</a>
+                            </li>
+                        </ul>
+                    </div>
+                    <div class="col-md-4 txt-right">
+                        <a href="https://discord.com/oauth2/authorize?response_type=code&client_id=944735010311786537&scope=identify%20guilds&state=15773059ghq9183habn&redirect_uri=http%3A%2F%2Flocalhost%3A3000&prompt=consent
+    ">
+                            <button type="button" className="btn btn-success" >
+                                <img src={discordLogo} height='20px'/>
+                                <span>Login</span>
+                            </button>
+                        </a>
+                    </div>
                 </div>
             </div>
+
         )
     }
 }
