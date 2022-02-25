b4339e55d67facfcd92960733ced5dfa729dcbdb
Nathan Wallace <nwallace6988@gmail.com>
2022-02-25 00:03:31 -0700

    add defaults to userModel.js

--- server/models/userModel.js
+++ server/models/userModel.js
@@ -1,15 +1,15 @@
 const db = require('../services/dbService')
 
 const userSchema = db.mongoose.Schema({
-    id: String,
-    user_name: String,
-    is_admin: Boolean,
-    avatar: String,
-    last_login: Number,
+    id: {type: String, required: true},
+    user_name: {type: String},
+    is_admin: {type: Boolean, default: false},
+    avatar: {type: String},
+    last_login: {type: Number},
     token: {
-        access_token: String,
-        expires_at: Number,
-        refresh_token: String,        
+        access_token: {type: String},
+        expires_at: {type: Number},
+        refresh_token: {type: String},        
     }
 });
 
