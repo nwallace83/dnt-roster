69a43862bda6f3d5178729c6ec999af3b4a36cdd
Nathan Wallace <nwallace6988@gmail.com>
2022-02-25 13:44:31 -0700

    more https fuckery

--- server/server.js
+++ server/server.js
@@ -26,7 +26,7 @@ app.use(
     })
 );
 
-app.use(helmet.crossOriginResourcePolicy({policy: "same-site"}))
+app.use(helmet.crossOriginResourcePolicy({policy: "cross-origin"}))
 
 helmet.contentSecurityPolicy.getDefaultDirectives
 
