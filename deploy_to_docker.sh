130ebb62a7eebcaaf44c0f77d8ac10496651ca06
Nathan Wallace <nwallace6988@gmail.com>
2022-02-25 04:46:28 -0700

    update build scripts for production

--- deploy_to_docker.sh
+++ deploy_to_docker.sh
@@ -15,4 +15,7 @@ docker run -d --network dntroster --hostname dntroster \
 	--env CLIENT_SECRET=$CLIENT_SECRET \
 	--env REDIRECT_URI=$REDIRECT_URI \
 	--env JWT_KEY=$JWT_KEY \
-	--restart=always -p 3001:3001 --name dntroster dntroster
+	--env CA_BUNDLE=ca_bundle.crt \
+	--env DOMAIN_CRT=dntroster.com_2022.crt \
+	--env DOMAIN_KEY=dntroster.com.key \
+	--restart=always -p 8443:8443 -p 3001:3001 --name dntroster dntroster
