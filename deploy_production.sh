a3fccc16950810806b8eec94574731d713e2c668
Nathan Wallace <nwallace6988@gmail.com>
2022-02-27 16:40:14 -0700

    update seed users, fix typo in deploy production script

--- deploy_production.sh
+++ deploy_production.sh
@@ -10,11 +10,11 @@ cp -Rf ./build/* ./server/html
 docker build --rm -f Dockerfile.prod -t dntroster-prod:temp .
 for I in `docker ps -a | grep dntroster-prod | awk '{print $1}'`;do docker stop $I;docker rm -f $I;done
 docker rmi dntroster-prod:latest
-docker tag dntroster-prod:temp dntroster-production:latest
+docker tag dntroster-prod:temp dntroster-prod:latest
 docker rmi dntroster-prod:temp
 docker run -d --network dntroster --hostname dntroster \
 	--env NODE_ENV=production \
 	--env CLIENT_SECRET=$CLIENT_SECRET \
 	--env REDIRECT_URI="https://dntroster.com" \
 	--env JWT_KEY=$JWT_KEY \
-	--restart=always -p 8443:8443 -p 8080:8080 --name dntroster-production dntroster-production
+	--restart=always -p 8443:8443 -p 8080:8080 --name dntroster-prod dntroster-prod
