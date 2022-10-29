b3df785e4d343f5944910b436076c93038500d13
Nathan Wallace <nwallace6988@gmail.com>
2022-10-29 00:30:08 -0600

    Revert to double rendering snapshot tests

--- src/__tests__/components/roster_test.js
+++ src/__tests__/components/roster_test.js
@@ -1,12 +1,13 @@
-import {fireEvent, prettyDOM, render, screen} from '@testing-library/react'
+import {fireEvent, render, screen} from '@testing-library/react'
 import '@testing-library/jest-dom'
 import Roster from '../../components/roster'
 import { Provider } from 'react-redux'
 import store from '../../store'
 import { setRoster } from '../../reducers/rosterSlice'
+import renderer from 'react-test-renderer';
 
 describe('Roster',() => {
-    let component
+    let container
 
     let testPlayers=[
         {characterName: 'test2', discordUserName: 'discordusername',primaryWeapon1: 'Life Staff', primaryWeapon2: 'Sword', 
@@ -27,8 +28,8 @@ describe('Roster',() => {
 
     beforeEach(() => {
         console.error = jest.fn()
-        let { container } = render(<Provider store={store}><Roster /></Provider>)
-        component = container
+        render(<Provider store={store}><Roster /></Provider>)
+        container = renderer.create(<Provider store={store}><Roster /></Provider>)
     })
 
     test('Headers are displayed',() => {
@@ -37,32 +38,32 @@ describe('Roster',() => {
         expect(screen.getByRole('columnheader',{name: /main/i})).toBeInTheDocument()
         expect(screen.getByRole('columnheader',{name: /alt/i})).toBeInTheDocument()
         expect(screen.getByRole('columnheader',{name: /active/i})).toBeInTheDocument()
-        expect(component.innerHTML).toMatchSnapshot()
+        expect(container.toJSON()).toMatchSnapshot()
     })
 
     test('Players are displayed',() => {
         expect(screen.getAllByRole('row').length).toEqual(4)
-        expect(component.innerHTML).toMatchSnapshot()
+        expect(container.toJSON()).toMatchSnapshot()
     })
 
     test('Players are sorted',() => {
         expect(screen.getAllByRole('row')[1].innerHTML).toContain('test1')
         expect(screen.getAllByRole('row')[2].innerHTML).toContain('test2')
         expect(screen.getAllByRole('row')[3].innerHTML).toContain('test3')
-        expect(component.innerHTML).toMatchSnapshot()
+        expect(container.toJSON()).toMatchSnapshot()
     })
 
     test('Show inactive players',() => {
         fireEvent.click(screen.getByRole('checkbox',{name: 'Show Inactive'}))
         expect(screen.getAllByRole('row').length).toEqual(5)
-        expect(component.innerHTML).toMatchSnapshot()
+        expect(container.toJSON()).toMatchSnapshot()
     })
 
     test('Filter players',() => {
             fireEvent.change(screen.getByLabelText(/filter:/i),{target: {value: 'discordusername'}})
             setTimeout(() => {
                 expect(screen.getAllByRole('row').length).toEqual(4)
-                expect(component.innerHTML).toMatchSnapshot()
+                expect(container.toJSON()).toMatchSnapshot()
             }, 300);
         
     })
