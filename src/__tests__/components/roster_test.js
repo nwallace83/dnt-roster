513bc32d9e295c6c9861e8e88793e2e2e815ac6c
Nathan Wallace <nwallace6988@gmail.com>
2022-10-28 23:52:51 -0600

    change snapshot tests to avoid duplicating components

--- src/__tests__/components/roster_test.js
+++ src/__tests__/components/roster_test.js
@@ -1,4 +1,4 @@
-import {fireEvent, render, screen} from '@testing-library/react'
+import {fireEvent, prettyDOM, render, screen} from '@testing-library/react'
 import '@testing-library/jest-dom'
 import Roster from '../../components/roster'
 import { Provider } from 'react-redux'
@@ -7,12 +7,13 @@ import { setRoster } from '../../reducers/rosterSlice'
 import renderer from 'react-test-renderer';
 
 describe('Roster',() => {
-    let container
+    let component
+
     let testPlayers=[
-        {characterName: 'test1', discordUserName: 'discordusername',primaryWeapon1: 'Life Staff', primaryWeapon2: 'Sword', 
+        {characterName: 'test2', discordUserName: 'discordusername',primaryWeapon1: 'Life Staff', primaryWeapon2: 'Sword', 
         primaryArmor: 'Light', primaryRole: 'dps', primaryGS: 600, secondaryWeapon1: 'Life Staff', secondaryWeapon2: 'Sword', 
         secondaryArmor: 'Light', secondaryRole: 'dps', secondaryGS: 600, inactive:false},
-        {characterName: 'test2', discordUserName: 'discordusername',primaryWeapon1: 'Life Staff', primaryWeapon2: 'Sword', 
+        {characterName: 'test1', discordUserName: 'discordusername',primaryWeapon1: 'Life Staff', primaryWeapon2: 'Sword', 
         primaryArmor: 'Light', primaryRole: 'dps', primaryGS: 600, secondaryWeapon1: 'Life Staff', secondaryWeapon2: 'Sword', 
         secondaryArmor: 'Light', secondaryRole: 'dps', secondaryGS: 600,inactive:false},
         {characterName: 'test3', discordUserName: 'discordusername',primaryWeapon1: 'Life Staff', primaryWeapon2: 'Sword', 
@@ -26,8 +27,9 @@ describe('Roster',() => {
     store.dispatch(setRoster(testPlayers))
 
     beforeEach(() => {
-        render(<Provider store={store}><Roster /></Provider>)
-        container = renderer.create(<Provider store={store}><Roster /></Provider>)
+        console.error = jest.fn()
+        let { container } = render(<Provider store={store}><Roster /></Provider>)
+        component = container
     })
 
     test('Headers are displayed',() => {
@@ -36,32 +38,33 @@ describe('Roster',() => {
         expect(screen.getByRole('columnheader',{name: /main/i})).toBeInTheDocument()
         expect(screen.getByRole('columnheader',{name: /alt/i})).toBeInTheDocument()
         expect(screen.getByRole('columnheader',{name: /active/i})).toBeInTheDocument()
-        expect(container.toJSON()).toMatchSnapshot()
+        expect(component.innerHTML).toMatchSnapshot()
     })
 
     test('Players are displayed',() => {
         expect(screen.getAllByRole('row').length).toEqual(4)
-        expect(container.toJSON()).toMatchSnapshot()
+        expect(component.innerHTML).toMatchSnapshot()
     })
 
     test('Players are sorted',() => {
         expect(screen.getAllByRole('row')[1].innerHTML).toContain('test1')
         expect(screen.getAllByRole('row')[2].innerHTML).toContain('test2')
         expect(screen.getAllByRole('row')[3].innerHTML).toContain('test3')
-        expect(container.toJSON()).toMatchSnapshot()
+        expect(component.innerHTML).toMatchSnapshot()
     })
 
     test('Show inactive players',() => {
         fireEvent.click(screen.getByRole('checkbox',{name: 'Show Inactive'}))
+        console.log(component.innerHTML)
         expect(screen.getAllByRole('row').length).toEqual(5)
-        expect(container.toJSON()).toMatchSnapshot()
+        expect(component.innerHTML).toMatchSnapshot()
     })
 
     test('Filter players',() => {
             fireEvent.change(screen.getByLabelText(/filter:/i),{target: {value: 'discordusername'}})
             setTimeout(() => {
                 expect(screen.getAllByRole('row').length).toEqual(4)
-                expect(container.toJSON()).toMatchSnapshot()
+                expect(component.innerHTML).toMatchSnapshot()
             }, 300);
         
     })
